2f6b5b0b91af4fe08e3f5a8ebf7ee97ebcf42324
Zeliwipin <zeliwipin@gmail.com>
2021-08-15 18:17:41 +0200

    mobx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { makeAutoObservable } from "mobx";
+import { observer } from "mobx-react";
+
+class Timer {
+  secondsPassed = 0;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  increaseTimer() {
+    this.secondsPassed += 1;
+  }
+}
+
+const myTimer = new Timer();
+
+const TimerView = observer(({ timer }) => (
+  <span>Seconds passed: {myTimer.secondsPassed}</span>
+));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TimerView />
+      <button onClick={() => myTimer.increaseTimer()}>
+        CLICK to increment
+      </button>
+      <h1>State :</h1>
     </div>
   );
 }
